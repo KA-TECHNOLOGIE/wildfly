5fe3559100c8254bb6e17cd6c6b0a5400303ceea
KA-TECHNOLOGIE <michael@katechnologie.fr>
2016-06-26 20:15:12 +0200

    Create new project: websocket

---
+++ websocket/src/main/java/fr/katechnologie/wildfly/websocket/App.java
@@ -0,0 +1,13 @@
+package fr.katechnologie.wildfly.websocket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
